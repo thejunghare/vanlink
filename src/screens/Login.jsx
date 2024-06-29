e79463838c2b592b0c01d61b73a332d86a6f5394
thejunghare <prasadjunghare2@gmail.com>
2024-06-29 11:23:06 +0530

    -add KeyboardAwareScrollView  package - fix ui distrub on input

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,55 +1,58 @@
-import React, { useState } from 'react';
-import { View, } from 'react-native';
-import { supabase } from '../lib/supabase';
-import { Text, TextInput, Button } from 'react-native-paper';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, {useState} from 'react';
+import {View, ScrollView} from 'react-native';
+import {supabase} from '../lib/supabase';
+import {Text, TextInput, Button} from 'react-native-paper';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
 
-const Login = ({ navigation }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Login = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleLogin = async () => {
-    const { error } = await supabase.auth.signInWithPassword({ email, password });
-    if (error) {
-      console.error(error);
-    }
-  };
+    const handleLogin = async () => {
+        const {error} = await supabase.auth.signInWithPassword({email, password});
+        if (error) {
+            console.error(error);
+        }
+    };
 
-  return (
-    <SafeAreaView className='h-screen w-11/12 m-auto flex flex-col justify-around'>
-        <View className=''>
-          <Text variant="headlineLarge" className='text-center'>Welcome back!</Text>
-          <Text variant="titleSmall" className='text-center'>Log in to your account</Text>
-        </View>
+    return (
+        <SafeAreaView>
+            <KeyboardAwareScrollView>
+                <View className='h-screen w-11/12 m-auto flex flex-col justify-around'>
+                    <View className=''>
+                        <Text variant="headlineLarge" className='text-center'>Welcome back!</Text>
+                        <Text variant="titleSmall" className='text-center'>Log in to your account</Text>
+                    </View>
 
-        <View>
-          <TextInput
-            label="Email"
-            value={email}
-            onChangeText={setEmail}
-            placeholder='Username'
-            left={<TextInput.Icon icon="account" />}
-          />
+                    <View>
+                        <TextInput
+                            label="Email"
+                            value={email}
+                            onChangeText={setEmail}
+                            placeholder='Username'
+                            left={<TextInput.Icon icon="account"/>}
+                        />
 
-          <TextInput
-            label="Password"
-            value={password}
-            onChangeText={setPassword}
-            placeholder='Password'
-            secureTextEntry
-            left={<TextInput.Icon icon="eye" />}
-          />
-        </View>
+                        <TextInput
+                            label="Password"
+                            value={password}
+                            onChangeText={setPassword}
+                            placeholder='Password'
+                            secureTextEntry
+                            left={<TextInput.Icon icon="eye"/>}
+                        />
+                    </View>
 
-        <View>
-          <Button icon="login" mode="contained" onPress={handleLogin}>
-            Log In
-          </Button>
-        </View>
-
-        {/* <Button title="Go to Signup" onPress={() => navigation.navigate('Signup')} /> */}
-    </SafeAreaView>
-  );
+                    <View>
+                        <Button icon="login" mode="contained" onPress={handleLogin}>
+                            Log In
+                        </Button>
+                    </View>
+                </View>
+            </KeyboardAwareScrollView>
+        </SafeAreaView>
+    );
 };
 
 export default Login;

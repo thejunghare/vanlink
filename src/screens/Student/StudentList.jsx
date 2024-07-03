5a2d21eb3a02710214e2dd446f16cfb77489ce9d
thejunghare <prasadjunghare2@gmail.com>
2024-07-03 15:06:13 +0530

    - add refresh list

--- src/screens/Student/StudentList.jsx
+++ src/screens/Student/StudentList.jsx
@@ -1,45 +1,58 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Text, Button, DataTable, IconButton, Searchbar} from 'react-native-paper';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {useNavigation} from '@react-navigation/native';
+import { View, RefreshControl, ScrollView, ToastAndroid } from 'react-native';
+import { Text, Button, DataTable, IconButton, Searchbar } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import { supabase } from '../../lib/supabase';
 
 const StudentList = () => {
     const navigation = useNavigation();
     const [searchQuery, setSearchQuery] = React.useState('');
     const [page, setPage] = React.useState(0);
     const [numberOfItemsPerPageList] = React.useState([2, 3, 4]);
-    const [itemsPerPage, onItemsPerPageChange] = React.useState(
-        numberOfItemsPerPageList[0]
-    );
+    const [itemsPerPage, onItemsPerPageChange] = React.useState(numberOfItemsPerPageList[0]);
+    const [students, setStudents] = React.useState([]);
+    const [refreshing, setRefreshing] = React.useState();
 
-    const [items] = React.useState([
-        {
-            key: 1,
-            name: 'Dhiraj Pawar',
-        },
-        {
-            key: 2,
-            name: 'shradha Maam',
-        },
-        {
-            key: 3,
-            name: 'Paddy 😄',
-        },
-    ]);
+    const fetchStudentDetails = async () => {
+        let { data: students, error } = await supabase
+            .from('students')
+            .select('*')
+            .eq('driver_id', 3);
 
-    const from = page * itemsPerPage;
-    const to = Math.min((page + 1) * itemsPerPage, items.length);
+        if (!error) {
+            //console.info('Student details fetched', students);
+            setStudents(students);
+            ToastAndroid.show('Student Fetched !', ToastAndroid.SHORT);
+        } else {
+            //console.error('Error while fetching student details', error);
+            ToastAndroid.show('Error while fetching students !', ToastAndroid.SHORT);
+        }
+    }
+
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        setTimeout(() => {
+            fetchStudentDetails().finally(() => setRefreshing(false));
+        }, 2000);
+    })
 
     React.useEffect(() => {
         setPage(0);
+        fetchStudentDetails();
     }, [itemsPerPage]);
 
+    const from = page * itemsPerPage;
+    const to = Math.min((page + 1) * itemsPerPage, students.length);
+
     return (
-        <View className={'flex-1'}>
+        <ScrollView
+            className={'flex-1'}
+            refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }>
             <View className={'m-3'}>
                 <Searchbar
-                    placeholder="Search Student"
+                    placeholder='Search Student'
                     onChangeText={setSearchQuery}
                     value={searchQuery}
                 />
@@ -51,26 +64,26 @@ const StudentList = () => {
                     <DataTable.Title numeric>Actions</DataTable.Title>
                 </DataTable.Header>
 
-                {items.slice(from, to).map((item) => (
-                    <DataTable.Row key={item.key}>
-                        <DataTable.Cell>{item.name}</DataTable.Cell>
+                {students.slice(from, to).map((student) => (
+                    <DataTable.Row key={student.id}>
+                        <DataTable.Cell>{student.student_fees}</DataTable.Cell>
                         <DataTable.Cell numeric>
-                            <IconButton icon="eye"
-                                        onPress={() => navigation.navigate('Student Details', item.key)}/>
+                            <IconButton icon='eye'
+                                onPress={() => navigation.navigate('Student Details', student.id)} />
                             <IconButton
-                                icon="pencil"
-                                onPress={() => navigation.navigate('Add Vehicle', {itemKey: item.key})}
+                                icon='pencil'
+                                onPress={() => navigation.navigate('Add Student', { itemKey: student.id })}
                             />
-                            <IconButton icon="delete" onPress={() => console.log('Delete', item.key)}/>
+                            <IconButton icon='delete' onPress={() => console.log('Delete', student.id)} />
                         </DataTable.Cell>
                     </DataTable.Row>
                 ))}
 
                 <DataTable.Pagination
                     page={page}
-                    numberOfPages={Math.ceil(items.length / itemsPerPage)}
+                    numberOfPages={Math.ceil(students.length / itemsPerPage)}
                     onPageChange={(page) => setPage(page)}
-                    label={`${from + 1}-${to} of ${items.length}`}
+                    label={`${from + 1}-${to} of ${students.length}`}
                     numberOfItemsPerPageList={numberOfItemsPerPageList}
                     numberOfItemsPerPage={itemsPerPage}
                     onItemsPerPageChange={onItemsPerPageChange}
@@ -78,12 +91,15 @@ const StudentList = () => {
                     selectPageDropdownLabel={'Rows per page'}
                 />
             </DataTable>
-            <View className='m-5 flex items-center justify-center'>
-                <Text variant='titleMedium' className='py-3'>No data exist click to add!</Text>
-                <Button icon='plus' mode='contained' onPress={() => navigation.navigate('Add Student')}>Add
-                    Student</Button>
-            </View>
-        </View>
+
+            {students.length === 0 && (
+                <View className='m-5 flex items-center justify-center'>
+                    <Text variant='titleMedium' className='py-3'>No data exist click to add!</Text>
+                    <Button icon='plus' mode='contained' onPress={() => navigation.navigate('Add Student')}>Add
+                        Student</Button>
+                </View>
+            )}
+        </ScrollView>
     );
 };
 

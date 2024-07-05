b1dd2bc530070d0edb71f646293de048982ec59f
thejunghare <prasadjunghare2@gmail.com>
2024-07-05 18:41:34 +0530

    - add loading effect to login page

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,18 +1,23 @@
-import React, {useState} from 'react';
-import {View, ScrollView} from 'react-native';
-import {supabase} from '../lib/supabase';
-import {Text, TextInput, Button} from 'react-native-paper';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
+import React from 'react';
+import { View, ToastAndroid } from 'react-native';
+import { supabase } from '../lib/supabase';
+import { Text, TextInput, Button } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
-const Login = ({navigation}) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const Login = ({ navigation }) => {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [loading, setLoading] = React.useState(false);
 
     const handleLogin = async () => {
-        const {error} = await supabase.auth.signInWithPassword({email, password});
+        setLoading(true);
+
+        const { error } = await supabase.auth.signInWithPassword({ email, password });
         if (error) {
-            console.error(error);
+            // console.error(error);
+            setLoading(false);
+            ToastAndroid.show('Invalid username or password', ToastAndroid.SHORT);
         }
     };
 
@@ -21,34 +26,34 @@ const Login = ({navigation}) => {
             <KeyboardAwareScrollView>
                 <View className='h-screen w-11/12 m-auto flex flex-col justify-around'>
                     <View className=''>
-                        <Text variant="headlineLarge" className='text-center'>Welcome back!</Text>
-                        <Text variant="titleSmall" className='text-center'>Log in to your account</Text>
+                        <Text variant='headlineLarge' className='text-center'>Welcome back!</Text>
+                        <Text variant='titleSmall' className='text-center'>Log in to your account</Text>
                     </View>
 
                     <View>
                         <TextInput
-                            label="Email"
+                            label='Email'
                             value={email}
                             onChangeText={setEmail}
                             placeholder='Username'
-                            textContentType="emailAddress"
-                            keyboardType="email-address"
-                            left={<TextInput.Icon icon="account"/>}
+                            textContentType='emailAddress'
+                            keyboardType='email-address'
+                            left={<TextInput.Icon icon='account' />}
                         />
 
                         <TextInput
-                            label="Password"
+                            label='Password'
                             value={password}
                             onChangeText={setPassword}
                             placeholder='Password'
                             secureTextEntry
-                            left={<TextInput.Icon icon="eye"/>}
+                            left={<TextInput.Icon icon='eye' />}
                         />
                     </View>
 
                     <View>
-                        <Button icon="login" mode="contained" onPress={handleLogin}>
-                            Log In
+                        <Button icon='login' mode='contained' onPress={handleLogin}>
+                           {loading? 'Logging In...' : 'Log React.In'}
                         </Button>
                     </View>
                 </View>

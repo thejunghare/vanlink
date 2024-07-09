15e3f1d827471b976d5759c96276e82d5c104d1b
thejunghare <prasadjunghare2@gmail.com>
2024-07-09 16:21:56 +0530

    - fix driver icon - fix maintenance icon - fix id share issue for owner

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -4,11 +4,11 @@ import { supabase } from '../lib/supabase';
 import { Searchbar, Button, Text, IconButton } from 'react-native-paper';
 import { StatusBar } from 'expo-status-bar';
 import driverIcon from '../../assets/icons/driver.png';
-import vanIcon from '../../assets/icons/van.png';
+import vanMaintenanceIcon from '../../assets/icons/van.png';
 import schoolIcon from '../../assets/icons/school.png';
 import studentIcon from '../../assets/icons/students.png';
 import moneyIcon from '../../assets/icons/money.png';
-import maintenanceIcon from '../../assets/icons/maintenance.png';
+import vehicleIcon from '../../assets/icons/vehicle.png';
 
 const Dashboard = ({ navigation }) => {
     const [searchQuery, setSearchQuery] = React.useState('');
@@ -19,12 +19,12 @@ const Dashboard = ({ navigation }) => {
     const [ownerId, setOwnerId] = React.useState('');
     const [driverId, setDriverId] = React.useState('');
     const [ownerWithDriverId, setOwnerWithDriverId] = React.useState('');
-    const CustomVehicleIcon = () => (<Image source={vanIcon} style={{ width: 60, height: 60 }} />);
+    const CustomVehicleIcon = () => (<Image source={vehicleIcon} style={{ width: 60, height: 60 }} />);
     const CustomDriverIcon = () => (<Image source={driverIcon} style={{ width: 60, height: 60 }} />);
     const CustomSchoolIcon = () => (<Image source={schoolIcon} style={{ width: 60, height: 60 }} />);
     const CustomStudentIcon = () => (<Image source={studentIcon} style={{ width: 60, height: 60 }} />);
     const CustomMoneyIcon = () => (<Image source={moneyIcon} style={{ width: 60, height: 60 }} />);
-    const CustomMaintenanceIcon = () => (<Image source={maintenanceIcon} style={{ width: 60, height: 60 }} />);
+    const CustomMaintenanceIcon = () => (<Image source={vanMaintenanceIcon} style={{ width: 60, height: 60 }} />);
 
     // fetch user detials here
     const fetchUserData = async () => {
@@ -120,7 +120,7 @@ const Dashboard = ({ navigation }) => {
                 />
 
                 {/* recents button */}
-                <View className='w-screen my-5 flex flex-row items-center justify-evenly'>
+                <View className='w-4/5 m-auto my-5 flex flex-row items-center justify-evenly'>
                     <Button icon="arrow-top-right" mode="outlined" onPress={() => console.log('Pressed')}>
                         Profile
                     </Button>
@@ -132,11 +132,11 @@ const Dashboard = ({ navigation }) => {
                 <Text variant='titleMedium' className='ml-5'>Management Dashboard</Text>
 
                 {/* logged in user details only for devlopment purpose */}
-                <View className='flex flex-row items-center justify-start'>
+               {/*  <View className='flex flex-row items-center justify-start'>
                     <Text variant='titleMedium' className='ml-5'>user ID: {userRole}</Text>
                     <Text variant='titleMedium' className='ml-5'>owner ID: {ownerId}</Text>
                     <Text variant='titleMedium' className='ml-5'>Role ID: {userRole}</Text>
-                </View>
+                </View> */}
 
                 {/* owner dashboard start */}
                 {userRole === 2 && (
@@ -175,7 +175,7 @@ const Dashboard = ({ navigation }) => {
                             <IconButton
                                 icon={CustomStudentIcon}
                                 size={60}
-                                onPress={() => navigation.navigate('Student List', { userId: userId, roleId: userRole })}
+                                onPress={() => navigation.navigate('Student List', { userId: userId, roleId: userRole, ownerId: ownerId })}
                                 accessibilityLabel='Student'
                             />
                         </View>
@@ -183,7 +183,7 @@ const Dashboard = ({ navigation }) => {
                             <IconButton
                                 icon={CustomMoneyIcon}
                                 size={60}
-                                onPress={() => navigation.navigate('Payment Details', { userId: userId, roleId: userRole })}
+                                onPress={() => navigation.navigate('Payment Details', { userId: userId, roleId: userRole, ownerId: ownerId })}
                                 accessibilityLabel='Payment'
                             />
                         </View>
@@ -191,7 +191,7 @@ const Dashboard = ({ navigation }) => {
                             <IconButton
                                 icon={CustomMaintenanceIcon}
                                 size={60}
-                                onPress={() => navigation.navigate('Vehicel Maintenance Record List', { userId: userId, roleId: userRole })}
+                                onPress={() => navigation.navigate('Vehicel Maintenance Record List', { userId: userId, roleId: userRole, ownerId: ownerId })}
                                 accessibilityLabel='Extra Icon'
                             />
                         </View>
